017e8e93b7ba3fe9ad129cc2afd782bc47739bce
kelliott <katherine.elliott@kwhanalytics.com>
2018-03-17 18:42:33 -0700

    Added precinct_shapefiles method but I dont know if it will work yet

--- shapefilegetter/app.py
+++ shapefilegetter/app.py
@@ -13,28 +13,23 @@ def hello_world():
     return 'Hello, World!'
 
 
-
 @app.route('/my-route')
 def my_route():
   page = request.args.get('page', default = 1, type = int)
   filter = request.args.get('filter', default = '*', type = str)
 
 
-
-app.route('/endpoint'):
-def endpoint():
-    request.args.get('state', '')
-
-
-
-def create_query(state=None):
+@app.route('/shapefiles/precinct'):
+def precinct_shapefiles():
+    geoid = request.args.get('geoid', default=None, type=str)
     precinct = Table('precinct_view_2012', metadata, autoload=True)
-    return
+    if geoid is not None:
+        precinct = precinct.select(precinct.c.geoid == geoid)
 
-
-
-
-
+    result = precinct.execute()
+    rows = result.fetchall()
+    # except we want to make sure these are json formatted
+    return rows 
 
 @app.route('/demo_response')
 def demo_response():
--- shapefilegetter/utils.py
+++ shapefilegetter/utils.py
@@ -15,7 +15,7 @@ def create_view():
     create = """CREATE VIEW precinct_view_2012 AS
         SELECT v.gid, v.statefp10, v.countyfp10, v.vtdst10, v.geoid10,
             s.name AS state_name, c.name AS county_name,
-            v.namelsad10 AS precinct_name, v.the_geom
+            v.namelsad10 AS precinct_name, ST_AsGeoJSON(v.the_geom)
         FROM tiger2012.vtd v
         JOIN tiger2012.county c ON v.statefp10 = c.statefp
             AND v.countyfp10 = c.countyfp
