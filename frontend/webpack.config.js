81f821efacce91bdc8a86c6486e06f99e1e29f46
Bhaskar Mookerji <mookerji@gmail.com>
2018-03-18 11:22:21 -0700

    Formatting changes.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,26 +2,26 @@
 // delete the local development overrides at the bottom of this file
 
 // avoid destructuring for older Node version support
-const resolve = require('path').resolve;
-const webpack = require('webpack');
+const resolve = require("path").resolve;
+const webpack = require("webpack");
 
 const CONFIG = {
   entry: {
-    app: resolve('./app.js')
+    app: resolve("./app.js")
   },
 
-  devtool: 'source-map',
+  devtool: "source-map",
 
   module: {
     rules: [
       {
         // Compile ES2015 using buble
         test: /\.js$/,
-        loader: 'buble-loader',
-        include: [resolve('.')],
+        loader: "buble-loader",
+        include: [resolve(".")],
         exclude: [/node_modules/],
         options: {
-          objectAssign: 'Object.assign'
+          objectAssign: "Object.assign"
         }
       }
     ]
@@ -30,13 +30,14 @@ const CONFIG = {
   resolve: {
     alias: {
       // From mapbox-gl-js README. Required for non-browserify bundlers (e.g. webpack):
-      'mapbox-gl$': resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
+      "mapbox-gl$": resolve("./node_modules/mapbox-gl/dist/mapbox-gl.js")
     }
   },
 
   // Optional: Enables reading mapbox token from environment variable
-  plugins: [new webpack.EnvironmentPlugin(['MapboxAccessToken'])]
+  plugins: [new webpack.EnvironmentPlugin(["MapboxAccessToken"])]
 };
 
 // This line enables bundling against src in this repo rather than installed deck.gl module
-module.exports = env => (env ? require('../webpack.config.local')(CONFIG)(env) : CONFIG);
+module.exports = env =>
+  env ? require("../webpack.config.local")(CONFIG)(env) : CONFIG;
